fd2af0759de1ec01ebbc8875adeedf87468a19b4
Reed Martz <reed@reedmartz.com>
2016-10-22 20:34:19 -0400

    Add basic model structure for option hierarchy

--- django/categorizer/models.py
+++ django/categorizer/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    label = models.CharField(max_length=128)
+
+
+class Topic(models.Model):
+    label = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Option(models.Model):
+    label = models.CharField(max_length=128)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    topics = models.ManyToManyField(Topic)
