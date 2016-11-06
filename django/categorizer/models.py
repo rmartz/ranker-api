fa0f7baca1266e4999cf43418e0702e85cba8cf7
Reed Martz <reed@reedmartz.com>
2016-11-06 08:04:43 -0500

    Add unique constraints on labels

--- django/categorizer/models.py
+++ django/categorizer/models.py
@@ -4,15 +4,21 @@ from django.db import models
 
 
 class Category(models.Model):
-    label = models.CharField(max_length=128)
+    label = models.CharField(max_length=128, unique=True)
 
 
 class Topic(models.Model):
     label = models.CharField(max_length=128)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = (("category", "label"),)
+
 
 class Option(models.Model):
     label = models.CharField(max_length=128)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     topics = models.ManyToManyField(Topic, related_name='options')
+
+    class Meta:
+        unique_together = (("category", "label"),)
