1696bf8cba6910663777be3cd27889127774c3f4
Reed Martz <reed@reedmartz.com>
2017-11-12 15:54:07 -0500

    Refactor tests to reduce matching constants

--- django/auth/tests/test_views.py
+++ django/auth/tests/test_views.py
@@ -1,15 +1,20 @@
-from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
 
 class TopicApiTestCase(APITestCase):
     def test_register_success(self):
-        response = self.client.post(
-            '/auth/user/',
-            {'username': 'test',
-             'email': 'test@example.com',
-             'password': 'password'})
+        user_creds = {
+            'username': 'test',
+            'email': 'test@example.com',
+            'password': 'password'
+        }
+
+        url = reverse('auth_user')
+        response = self.client.post(url, user_creds)
 
         self.assertEqual(response.status_code, 201)
 
@@ -18,37 +23,40 @@ class TopicApiTestCase(APITestCase):
         user_class = get_user_model()
         user = user_class.objects.get()
         self.assertEqual(response_json, {'token': user.auth_token.key})
-        self.assertEqual(user.email, 'test@example.com')
-        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.email, user_creds['email'])
+        self.assertEqual(user.username, user_creds['username'])
 
 
 class TokenApiTestCase(APITestCase):
     def setUp(self):
         user_class = get_user_model()
 
-        self.user = user_class.objects.create_user('user', 'user@example.com',
-                                                   'password')
+        self.user_cred = {
+            'username': 'user',
+            'password': 'password'
+        }
+
+        self.user = user_class.objects.create_user(email='user@example.com',
+                                                   **self.user_cred)
         self.token = Token.objects.create(user=self.user)
 
     def test_get_token(self):
-        response = self.client.post('/auth/user/token',
-                                    {'username': 'user',
-                                     'password': 'password'})
+        url = reverse('auth_token')
+        response = self.client.post(url, self.user_cred)
         self.assertEqual(response.status_code, 200)
 
         response_json = response.json()
         self.assertEqual(response_json['token'], self.token.key)
 
     def test_get_token_failure(self):
-        response = self.client.post('/auth/user/token',
-                                    {'username': 'user',
-                                     'password': 'badpass'})
+        url = reverse('auth_token')
+        response = self.client.post(url,
+                                    dict(self.user_cred, password='badpass'))
         self.assertEqual(response.status_code, 403)
 
     def test_token_reset(self):
-        response = self.client.delete('/auth/user/token',
-                                      {'username': 'user',
-                                       'password': 'password'})
+        url = reverse('auth_token')
+        response = self.client.delete(url, self.user_cred)
         self.assertEqual(response.status_code, 200)
 
         response_json = response.json()
@@ -58,7 +66,7 @@ class TokenApiTestCase(APITestCase):
         self.assertNotEqual(response_json['token'], new_token)
 
     def test_token_failure(self):
-        response = self.client.delete('/auth/user/token',
-                                      {'username': 'user',
-                                       'password': 'badpass'})
+        url = reverse('auth_token')
+        response = self.client.delete(url,
+                                      dict(self.user_cred, password='badpass'))
         self.assertEqual(response.status_code, 403)
--- django/auth/urls.py
+++ django/auth/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^user/?$', views.user_admin),
-    url(r'^user/token/?$', views.token_admin),
+    url(r'^user/?$', views.user_admin, name='auth_user'),
+    url(r'^user/token/?$', views.token_admin, name='auth_token'),
 ]
